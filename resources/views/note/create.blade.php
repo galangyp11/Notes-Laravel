22ebe06c0971b9b6d6f66cb5324d26c9028a3314
Galangyp <galangy11@gmail.com>
2024-12-02 23:58:16 +0700

    first commit

--- resources/views/note/create.blade.php
+++ resources/views/note/create.blade.php
@@ -9,7 +9,7 @@
             @csrf
             <textarea name="note" class="bg-white p-4 row-span-5 rounded-md w-full h-full resize-none" id="" cols="30"  rows="10" placeholder="Type Note Here... "></textarea>
             <div class="row-span-1 flex justify-center items-end">
-                <button class="bg-blue-300 text-    blue-700 font-semibold rounded-md px-4 py-1 w-40">Submit</button>
+                <button class="bg-blue-300 text-blue-700 font-semibold rounded-md px-4 py-1 w-40">Submit</button>
             </div>
         </form>
        
